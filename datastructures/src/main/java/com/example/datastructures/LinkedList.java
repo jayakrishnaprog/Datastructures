1adc730eb169f6dca0d09b180b6a11e99c9d40b0
unknown <jayakrishna0520@gmail.com>
2024-06-18 21:10:50 +0530

    Linkedlist commit

--- datastructures/src/main/java/com/example/datastructures/LinkedList.java
+++
@@ -1,5 +0,0 @@
-package com.example.datastructures;
-
-public class LinkedList {
-
-}
---
+++ datastructures/src/main/java/com/prepare/datstructures/LinkedList.java
@@ -0,0 +1,6 @@
+package com.prepare.datstructures;
+
+public class LinkedList {
+	//A linear data structure used to store the elements in contiguous locations is called a Linked List in Java.
+	//It has addresses and pointers that are used to link the elements, and each element in the linked list consists of two parts, namely the data part and the address part
+}
